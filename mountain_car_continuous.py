df95edd39937efb2fead7a12ae2d4f0dd23d85c9
r3b0rn <yakovinart123@gmail.com>
2024-12-20 16:41:42 +0300

    отображение попыток на каждой эпохе

--- mountain_car_continuous.py
+++ mountain_car_continuous.py
@@ -1,19 +1,29 @@
+from math import ceil
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
-import matplotlib
 
-matplotlib.use("TkAgg")
+from stable_baselines3.common.callbacks import BaseCallback
 
 
-def animate(model, save=False):
-    import matplotlib.pyplot as plt
-    from matplotlib.animation import FuncAnimation, FFMpegWriter
+class VideoRecordingCallback(BaseCallback):
+    def __init__(self, animate_on_every: int|float, verbose=1):
+        super(VideoRecordingCallback, self).__init__(verbose)
+        self.freq = ceil(animate_on_every)
+        self.epoch = 1
 
+    def _on_step(self) -> bool:
+        if self.n_calls % self.freq == 0:
+            animate(self.model, show=False, save=True, epoch=self.epoch)
+            self.epoch += 1
+        return True
+
+
+def interact_wit_env(model):
+    frames = []
     eval_env = gym.make(ENV_ID, render_mode="rgb_array")
 
     obs, _ = eval_env.reset()
-    frames = []
     total_reward = 0
 
     max_steps = 1000
@@ -26,8 +36,21 @@ def animate(model, save=False):
         frames.append(frame)
 
         if done or truncated:
-            print(f'{total_reward = }')
             break
+
+    eval_env.close()
+    print(total_reward)
+
+    return total_reward, frames
+
+
+def animate(model, show=True, save=False, epoch=0):
+    import matplotlib.pyplot as plt
+    from matplotlib.animation import FuncAnimation, FFMpegWriter
+
+    filename = 'animation.mp4' if not epoch else f'animation_{epoch}.mp4'
+
+    total_reward, frames = interact_wit_env(model)
 
     fig, ax = plt.subplots()
     ax.axis('off')
@@ -40,11 +63,12 @@ def animate(model, save=False):
     ani = FuncAnimation(fig, update, frames=frames, blit=True, interval=10)
 
     if save:
-        ani.save("animation.mp4", writer=FFMpegWriter(fps=60))
+        ani.save(filename, writer=FFMpegWriter(fps=60))
 
-    plt.show()
-
-    eval_env.close()
+    if show:
+        plt.show()
+    else:
+        plt.close(fig)
 
 
 def learn(save=False):
@@ -67,18 +91,19 @@ def learn(save=False):
         n_epochs=10,
         batch_size=n_envs*n_steps,
         tensorboard_log="./ppo_mountaincar_tensorboard/",
-        verbose=1,
+        verbose=0,
         device='cuda'
     )
 
-    model.learn(total_timesteps=500_000)
+    model.learn(total_timesteps=n_envs*n_steps*489, callback=VideoRecordingCallback(animate_on_every=489*n_steps/10))
+
     if save:
         model.save("ppo_mountaincar")
 
     return model
 
 
-LEARN = False
+LEARN = True
 ENV_ID = "MountainCarContinuous-v0"
 
 
